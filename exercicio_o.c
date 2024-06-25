2378269552c2d3a347f7962405107d42016aa412
Luiz-Quatrini <luiz.quatrini@etec.sp.gov.br>
2024-06-25 14:55:59 -0300

    Add files via upload

---
+++ exercicio_o.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,d,P,S;
+    printf("Escreva o numero A: ");
+    scanf("%d", &a);
+    printf("Escreva o numero B: ");
+    scanf("%d", &b);
+    printf("Escreva o numero C: ");
+    scanf("%d", &c);
+    printf("Escreva o numero D: ");
+    scanf("%d", &d);
+    P=a*c;
+    S=b+d;
+    printf("A Variavel P eh: %d\n", P);
+    printf("A Variavel S eh: %d\n", S);
+}
